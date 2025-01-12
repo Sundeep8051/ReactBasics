bb2d66442816bf8213320360fa5dfba57db0f54f
Sundeep Manibhushan <sundeep.manibhushan@gmail.com>
2025-01-12 21:42:09 +0530

    added Acordion component

--- src/components/ReusableComponents/Accordion/AccordionComponent.jsx
+++ src/components/ReusableComponents/Accordion/AccordionComponent.jsx
@@ -1,5 +1,41 @@
-const AccordionComponent = () => {
-  return <div>AccordionComponent</div>;
+import { useState } from "react";
+import { GoChevronDown, GoChevronRight } from "react-icons/go";
+
+/* eslint-disable react/prop-types */
+const AccordionComponent = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleShow = (index) => {
+    const idx = (currentIdx) => (currentIdx === index ? -1 : index);
+    setExpandedIndex(idx);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    const icon = (
+      <span className="text-xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronRight />}{" "}
+      </span>
+    );
+    return (
+      <div key={item.id}>
+        <h2
+          onClick={() => handleShow(index)}
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+        >
+          {item.label} {icon}
+        </h2>
+        {isExpanded && <p className="p-5 border-b">{item.content}</p>}
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div>AccordionComponent</div>
+      <div>{renderedItems}</div>
+    </>
+  );
 };
 
 export default AccordionComponent;
--- src/components/ReusableComponents/Accordion/AccordionPage.jsx
+++ src/components/ReusableComponents/Accordion/AccordionPage.jsx
@@ -1,9 +1,30 @@
 import AccordionComponent from "./AccordionComponent";
 
+const ListItems = [
+  {
+    id: 1,
+    label: "React",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus odit architecto beatae, excepturi cum illum accusantium ut sapiente temporibus distinctio aspernatur, quidem placeat iure ducimus perspiciatis repudiandae debitis officia expedita.",
+  },
+  {
+    id: 2,
+    label: "Vue",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi accusamus facere in ea, non tempore suscipit, necessitatibus unde doloremque quaerat autem architecto sequi at molestias, quibusdam dolorem minima perferendis error?",
+  },
+  {
+    id: 3,
+    label: "Angular",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex corrupti minima quia, ipsa quis dolores, omnis quam numquam provident harum consequatur qui sequi esse sunt praesentium, quas cum voluptate illum.",
+  },
+];
+
 const AccordionPage = () => {
   return (
     <div>
-      <AccordionComponent></AccordionComponent>
+      <AccordionComponent items={ListItems}></AccordionComponent>
     </div>
   );
 };
